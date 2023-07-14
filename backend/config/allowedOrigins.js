f0a4de783dd5ac70dfb85bfe0085fa957311e85e
sandarujayathilaka <sandarujayathilaka26@gmail.com>
2023-07-14 20:41:28 +0530

    added

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:8080",
   "http://localhost:3000",
+  "https://agofficeapis.onrender.com",
 ];
 
 module.exports = allowedOrigins;
