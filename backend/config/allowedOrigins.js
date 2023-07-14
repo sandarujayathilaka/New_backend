bd8f66a04d012833e43cc83547dce69a0412d0a8
sandarujayathilaka <sandarujayathilaka26@gmail.com>
2023-07-14 20:59:39 +0530

    added

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "https://sandarujayathilaka.github.io/frontend",
+  "https://sandarujayathilaka.github.io",
   "http://127.0.0.1:5500",
   "http://localhost:8080",
   "http://localhost:3000",
